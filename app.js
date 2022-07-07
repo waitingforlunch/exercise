e19cd16f3618543465273cfa6077f6322f67cd44
Yoon Jin  Kim <yoonjinkim@Yoonui-MacBookPro.local>
2022-07-07 22:24:29 +0900

    todayfinish

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const a = 34;
 const b = 2;
-const myName = "boa";
+let myName = "boa";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 
 console.log("hello " + myName); 
+
+myName = "nicolas";
+
+console.log("your new name is" + myName);
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek[4]);
+
+// Before adding sunday
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+//after adding sunday
+console.log(daysOfWeek);
