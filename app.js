d7c8cd8019315694ee50856264c9ab7b316d5db4
Yoon Jin  Kim <yoonjinkim@Yoonui-MacBookPro.local>
2022-07-15 17:43:07 +0900

    JavaScript #3

--- app.js
+++ app.js
@@ -25,3 +25,53 @@ daysOfWeek.push("sun");
 
 //after adding sunday
 console.log(daysOfWeek);
+
+//object 
+const player = {
+    name: "junyeob",
+    points: 10,
+    fat: true,
+};
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+
+console.log(player);
+player.fat = false;
+console.log(player);
+
+console.log(player);
+player.lastName = "kim";
+console.log(player);
+
+
+//funtion 함수
+function sayHello() {
+    console.log("Hello!");
+}
+
+sayHello();
+sayHello();
+sayHello();
+
+const player2 = {
+    name: "yoonjin",
+    sayHello: function (name) {
+        console.log("hello!" + name);
+    }
+}
+player2.sayHello("yoonjion");
+
+
+const month = {
+    name: "May",
+    daysInMonth: 31,
+    daysInWeek: ["mon", "tue", "wed", "thu", "fri", "sat", "sun"],
+    indexOfFirstDate: 6,
+    getDayOfDate: function (date) {
+        namuzi = date % 7;
+        console.log(this.daysInWeek[(this.indexOfFirstDate + namuzi) % 7]);
+    }
+}
+
+month.getDayOfDate(20);
