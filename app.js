893ab436bc737d6140e9c9044ff179d2906b2d77
Yoon Jin  Kim <yoonjinkim@Yoonui-MacBookPro.local>
2022-07-16 21:18:53 +0900

    javascript #3

--- app.js
+++ app.js
@@ -75,3 +75,34 @@ const month = {
 }
 
 month.getDayOfDate(20);
+
+
+//Return
+const calculator = {
+    plus: function (a, b) {
+        return (a + b);
+    },
+    minus: function (a, b) {
+        return (a - b);
+    },
+    times: function (a, b) {
+        return (a * b);
+    },
+    divide: function (a, b) {
+        return (a / b);
+    },
+    power: function (a, b) {
+        return (a ** b);
+    },
+}
+// 리턴 받은걸 다음 연산에서 활용
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+
+const age = 96;
+function calculateKrAge(ageOfForeigner) {
+    return ageOfForeigner + 2;
+}
+
+const KrAge = calculateKrAge(age);
